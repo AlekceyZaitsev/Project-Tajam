e4667f8082a6f4d3aa8f2f3c61baa71ac97af90f
AlekseyZaitcev <100513089+AlekseyZaitcev@users.noreply.github.com>
2022-03-01 17:01:18 +0300

    Финальное меню Header
    - Доработка стилей для меню.
    - Возможность добавлять пункты меню так, чтобы верстка не сломалась.
    - Полностью рабочая шапка для адаптива.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@
 
 
 const headerTop = document.querySelector('.header-top');
-const addClassBurgerHeader = document.querySelector('.header-top-menu-burger__icon');
+const addClassBurgerHeader = document.querySelector('.header-top-menu-list');
 
 function addClassToHeader(event) {
 
     if (event.target.closest('.header-top-menu-burger__icon')) {
         addClassBurgerHeader.classList.toggle('_active');
+        
     }
 
     if (!event.target.closest('.header-top')) {
